25d9f9a9627c3ab526903bf981660b08e40d199f
bitschrauber <mgropp@web.de>
2021-02-07 13:18:39 +0100

    Initial Commit

---
+++ bestfriends/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bestfriends
+//
+//  Created by Matthias Gropp on 07.02.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
